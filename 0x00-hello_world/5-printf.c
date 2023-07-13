8cf30ba83c2464cba07b52d3ed262c6d7246d425
Aboagye-max <135050867+Aboagye-max@users.noreply.github.com>
2023-07-13 18:38:33 +0000

    Update 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	char s[] = "with proper grammar, but the outcome is a piece of art,";
-	printf("%s\n",s);
-	return 0;
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
