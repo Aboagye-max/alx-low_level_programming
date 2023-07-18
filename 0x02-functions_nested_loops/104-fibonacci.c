f6f9f5d9b06d7d8148c16f9dd43e7b91e5515c05
Aboagye-max <maxwellaboagye130@gmail.com>
2023-07-18 15:02:47 -0700

    new files added

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ *
+ *Prints the first 98 numbers fo the fibonacci sequence
+ *
+ *return 0(success)
+ */
+int main(void)
+{
+	int sum = 1;
+
+	for(int i=1; i<=98; i++)
+	{
+		printf("%d, ",sum)
+		sum += i++;
+
+	}
+	printf("\n");
+	return (0);
+}
