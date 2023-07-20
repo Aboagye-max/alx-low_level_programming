654c5bfbf2654432d2b6febe912f3c58229bf93a
Aboagye-max <135050867+Aboagye-max@users.noreply.github.com>
2023-07-20 14:18:03 +0000

    Update 101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -9,20 +9,10 @@
   */
 void print_number(int n)
 {
-	unsigned int k = n;
-
-	if (n < 0)
+	int number ;
+	if( n % 2 == 0 )
 	{
-		n *= -1;
-		k = n;
-		_putchar('-');
+		_putchar(n);
 	}
-
-	k /= 10;
-
-	if (k != 0)
-		print_number(k);
-
-	_putchar((unsigned int) n % 10 + '0');
 
 }
