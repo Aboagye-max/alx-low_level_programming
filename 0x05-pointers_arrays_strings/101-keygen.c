22cb503df0b420e05cd87eab8ba7e39ef8c04dd5
Aboagye-max <135050867+Aboagye-max@users.noreply.github.com>
2023-07-24 17:26:37 +0000

    Update 101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -2,22 +2,28 @@
 #include <stdlib.h>
 #include <time.h>
 
+/**
+  * 101-keygen - generates random valid passwords for the program
+  * @int: The int to be treated
+  *
+  * Return: 0( sucess )
+  */
 void generatePassword(int len) {
    char possibleChars[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()";
    char password[len+1];
    
-   srand(time(0)); // seed for random number generation
+   srand(time(0)); 
    for(int i = 0; i < len; i++) {
       int randomIndex = rand() % (sizeof(possibleChars) - 1);
       password[i] = possibleChars[randomIndex];
    }
    
-   password[len] = '\0'; // null terminate the string
+   password[len] = '\0'; 
    printf("The randomly generated password is: %s\n", password);
 }
 
 int main() {
-   int len = 10; // desired length of password
+   int len = 10; 
    generatePassword(len);
    return 0;
 }
