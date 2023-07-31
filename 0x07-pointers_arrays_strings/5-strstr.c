23adb427b13ba5d5b7f68413fd384a133e8fd147
Aboagye-max <135050867+Aboagye-max@users.noreply.github.com>
2023-07-31 14:59:41 +0000

    Update 5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -11,17 +11,26 @@
 
 char *_strstr(char *haystack, char *needle)
 {
-		int i;
-			int s = 0;
+	int i;
+	int s = 0;
 
-				while (needle[s] != '\0')
-							s++;
+	while (needle[s] != '\0')
+		s++;
 
-					while (*haystack)
-							{
-										for (i = 0; needle[i]; i++)
-													{
-																	if (haystack[i] != needle[i])
+	while (*haystack)
+	{
+		for (i = 0; needle[i]; i++)
+		{
+			if (haystack[i] != needle[i])
+				break;
+		}
+		if (i != s)
+			haystack++;
+		else
+			return (haystack);
+	}
+	return (NULL);
+}										if (haystack[i] != needle[i])
 																						break;
 																			}
 												if (i != s)
