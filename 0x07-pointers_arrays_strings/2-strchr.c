51404b938f46638fe0b75c9eb4b21b9dc551908d
Aboagye-max <135050867+Aboagye-max@users.noreply.github.com>
2023-07-31 14:55:40 +0000

    Update 2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -14,15 +14,12 @@ char *_strchr(char *s, char c)
 	while (*s)
 	{
 		if (*s != c)
-		{
 			s++;
-		}else{
-		 return (s);
-		}
-		if (c == '\0')
-		{
+		else
 			return (s);
-		}
-		return (NULL);
 	}
+	if (c == '\0')
+		return (s);
+
+	return (NULL);
 }
