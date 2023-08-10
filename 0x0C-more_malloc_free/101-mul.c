dc54f74bd290628982d379c9f3377c6a1ad710b7
Aboagye-max <135050867+Aboagye-max@users.noreply.github.com>
2023-08-10 15:20:47 +0000

    Update 101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,1 +1,28 @@
-all is well
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+  * main - Multiplies two positive integers
+  * @argc: argument count
+  * @argv: argument vector
+  *
+  * Return: Always zero
+  */
+int main(int argc, char *argv[])
+{
+        if(argc > 3)
+        {
+                printf("Error\n");
+                exit(98);
+        }else{
+                int i;
+                int mul = 1;
+                for( i = 1; i < argc; i++ )
+                {
+                        int v = strtol(argv[i], NULL , 10);
+                        mul = mul * v;
+                }
+                printf("%d\n", mul);
+        }
+        return (0);
+}
