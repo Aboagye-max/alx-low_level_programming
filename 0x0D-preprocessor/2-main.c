b86a94b1826ce1588eb85e6bfd3fa0b7a5bd7893
Aboagye Maxwell <135050867+Aboagye-max@users.noreply.github.com>
2023-08-14 20:20:45 +0000

    Update 2-main.c

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,4 +1,17 @@
-#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
 /**
   * main - Prints the file name of the program
   *
